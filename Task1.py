a1795013b273416a943dfc70a975491d6d58355b
jenleap <jenleap@gmail.com>
2021-03-20 14:31:02 -0400

    completed task 1 and 2

--- Task1.py
+++ Task1.py
@@ -18,3 +18,17 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+def count_phone_numbers(records):
+    number_count = set()
+
+    for line in records:
+        number_count.add(line[0])
+        number_count.add(line[1])
+
+    return len(number_count)
+
+texts.extend(calls)
+unique_phone_nums = count_phone_numbers(texts)
+print("There are {} different telephone numbers in the records.".format(unique_phone_nums))
+
--- Task2.py
+++ Task2.py
@@ -20,3 +20,25 @@ Print a message:
 September 2016.".
 """
 
+def calculate_longest_call(call_list):
+    call_dict = dict()
+
+    for line in call_list:
+        for i in range(2):
+            if call_dict.get(line[i]):
+                call_dict[line[i]] += int(line[3])
+            else:
+                call_dict[line[i]] = int(line[3])
+        
+    longest_call = max(call_dict.values())
+
+    for key, value in call_dict.items():
+        if value == longest_call:
+            print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(key, value))
+
+calculate_longest_call(calls)
+
+
+
+
+
